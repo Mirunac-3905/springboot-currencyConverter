9b4c2c57d677a6a0848530fdc2ab9ab1cedbaca6
Miruna <miruna.c2023cse@sece.ac.in>
2025-07-16 19:18:48 +0530

    Initial commit

--- src/test/java/com/nerdiccoder/example/example/ExampleApplicationTests.java
+++ src/test/java/com/nerdiccoder/example/example/ExampleApplicationTests.java
@@ -1,13 +1,27 @@
+// package com.nerdiccoder.example.example;
+
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+// class ExampleApplicationTests {
+
+// 	@Test
+// 	void contextLoads() {
+// 	}
+
+// }
+
 package com.nerdiccoder.example.example;
 
+import com.nerdiccoder.currency.ExampleApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ExampleApplication.class)
 class ExampleApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
